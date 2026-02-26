32824d090b2392754592d863d026820bda720def
devrodolfo <rodolfocosta.ufg@gmail.com>
2026-02-26 11:18:27 -0300

    Criação de super USer

---
+++ create_superuser.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and password and not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
